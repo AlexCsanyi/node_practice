ef2542c07fb43d39580bb29f8963b3e43a5fefeb
AlexCsanyi <alex.csanyi@gmail.com>
2020-02-13 07:19:15 +0000

    serving static files with express

---
+++ express.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use("/public", express.static(path.join(__dirname, "main")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "main", "index.html"));
+});
+
+// examples
+// app.get("/example", (req, res) => {
+//   res.send("you reached the example route");
+// });
+
+// app.get("/example/:name/:age", (req, res) => {
+//   console.log(req.params);
+//   console.log(req.query);
+//   res.send(req.params.name + " : " + req.params.age);
+// });
+
+app.listen(3000);
