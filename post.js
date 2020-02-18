ef9f26f2aebe1867219723fbe725ed33ac23db3a
AlexCsanyi <alex.csanyi@gmail.com>
2020-02-18 07:08:59 +0000

    testing joi library

--- post.js
+++ post.js
@@ -1,19 +1,37 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const joi = require("joi");
 const app = express();
 
 app.use("/public", express.static(path.join(__dirname, "post")));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "post", "index.html"));
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body);
-  res.json({ success: true });
+  const schema = joi.object().keys({
+    email: joi
+      .string()
+      .trim()
+      .email()
+      .required(),
+    password: joi
+      .string()
+      .min(5)
+      .max(10)
+      .required()
+  });
+  joi.validate(req.body, schema, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("an error has occured");
+    }
+    console.log(result);
+    res.send("successfully posted data");
+  });
 });
 
 app.listen(3000);
