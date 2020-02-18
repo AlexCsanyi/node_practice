11e5d01dc1efc82c4875dc365f211a0a915374d5
AlexCsanyi <alex.csanyi@gmail.com>
2020-02-18 06:52:21 +0000

    json

--- post.js
+++ post.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
+
 app.use("/public", express.static(path.join(__dirname, "post")));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "post", "index.html"));
@@ -11,7 +13,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  res.send("successfully posted data");
+  res.json({ success: true });
 });
 
 app.listen(3000);
